8bee867db0d4bd03c62aec255e4239fb8e315695
tadaHrd <hrudka.tadeas@gmail.com>
2024-07-17 13:56:03 +0200

    changed docs

---
+++ docs/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["aes_rust",{"t":"CCHHHHHHKM","n":["anyway","to_bytes","check_separator","decode","decode_to_string","encode","encode_escape","encode_escaped","ToBytes","to_bytes"],"q":[[0,"aes_rust"],[2,"aes_rust::anyway"],[8,"aes_rust::to_bytes"],[10,"alloc::vec"],[11,"alloc::string"],[12,"core::str::error"],[13,"core::result"],[14,"core::convert"]],"i":[0,0,0,0,0,0,0,0,0,9],"f":"``{bd}{b{{h{f}}}}{b{{A`{j{n{l{h{f}}}}}}}}{{ce}jAb{{Ad{b}}}}{{ced}jAb{{Ad{b}}}}1`{Ab{{Af{f}}}}","D":"Af","p":[[1,"str"],[1,"bool"],[1,"u8"],[5,"Vec",10],[5,"String",11],[5,"Utf8Error",12],[1,"tuple"],[6,"Result",13],[10,"ToBytes",8],[10,"AsRef",14],[1,"slice"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
---
+++ docs/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["aes_rust",["",[],["anyway.rs","lib.rs","to_bytes.rs"]]]\
+]'));
+createSrcSidebar();
